87ac1c80356ea70c4010c83a87d31d31d8bb74a8
meewrage <luki_wop@mail.ru>
2023-03-23 16:36:24 +0300

    Использование рекурсии в методе для написания ряда чисел от n до 1

---
+++ ex_64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadNum(string msgToUser)
+{
+    Console.WriteLine(msgToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+void FromNto1(int n)
+{
+Console.Write(n + " ");
+if (n > 1) FromNto1(n - 1);
+}
+
+FromNto1(ReadNum("Введите число: "));
